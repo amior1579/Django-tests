2da1150ec9301f8d6dce337789870c8a8dd99156
amir79 <amir.shahvirdi79@gmail.com>
2023-04-29 12:16:13 +0330

    register form 1

--- authTest/views.py
+++ authTest/views.py
@@ -5,3 +5,14 @@ def home(request):
     return render(request, 'authTest/index.html',{
         
     })
+
+
+def login(request):
+    return render(request, 'authTest/login.html',{
+        
+    })
+
+def register(request):
+    return render(request, 'authTest/register.html',{
+
+    })
