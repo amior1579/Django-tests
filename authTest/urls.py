cb0d008e474a98c3fe6f1d5115b5e1eab5954d2c
amir79 <amir.shahvirdi79@gmail.com>
2023-04-29 16:57:45 +0330

    register form 2 (model)

--- authTest/urls.py
+++ authTest/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # urls
     path('', views.home, name='home'),
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
+
+    # server request
+    path('user_registration', views.user_registration, name='user_registration'),
 
 
 ]
--- authTest/views.py
+++ authTest/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
 
 def home(request):
@@ -16,3 +19,27 @@ def register(request):
     return render(request, 'authTest/register.html',{
 
     })
+
+
+def user_registration(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirmation = request.POST['confirmation']
+        username_exists = UserAuth.objects.values_list('username', flat=True)
+        
+        if confirmation != password:
+            return render(request, 'authTest/register.html',{
+                'message': ' !رمز عبور مطابقت ندارد, لطفا دوباره وارد کنید',
+            })  
+        elif username in username_exists is not None:
+            return render(request, 'authTest/register.html',{
+                'username_exists_message': 'کاربری با این نام کاربری از قبل وجود دارد',
+            })  
+        else:
+            user = UserAuth.objects.create_user(username,password)
+            user.save()
+            return HttpResponseRedirect(reverse('home'))
+        
+    else:
+        return render(request, 'authTest/register.html')
